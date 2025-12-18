2f4da12521ef94a06d535dc603eaa67411ea9e88
sergen <srgnunlu@icloud.com>
2025-12-18 16:57:23 +0300

    Initial Commit

---
+++ PolyglotReader/PolyglotReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolyglotReaderApp.swift
+//  PolyglotReader
+//
+//  Created by Sergen Ünlü on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PolyglotReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
